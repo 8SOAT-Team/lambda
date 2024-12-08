bf157dd558e9e26c06882ebc36968b8fe3e630c6
Fernanda Vecchiati Beato <feehvecch@gmail.com>
2024-12-08 10:52:42 -0300

    Update fastorderAuth.mjs
    Atualizando autorização com cpf e authorization

--- fastorderAuth.mjs
+++ fastorderAuth.mjs
@@ -1,19 +1,39 @@
-export const handler = async (event) => {
-  let authStatus = 'Allow';
+const defaultPassword = 'Fast*Order'; 
 
-  const response = {
-    policyDocument: { 
+export const handler = async (event) => {
+  const cpf = event.headers['cpf'];
+  const authorizationHeader = event.headers['Authorization'];
+
+  let policyDocument;
+  const principalId = 'user';
+
+  if (!cpf) {
+    policyDocument = generatePolicy(principalId, 'Allow', event.methodArn);
+  } else {
+    policyDocument = generatePolicy(principalId, 'Allow', event.methodArn);
+  }
+
+  if (authorizationHeader && authorizationHeader === `Basic ${Buffer.from(defaultPassword).toString('base64')}`) {
+    policyDocument = generatePolicy(principalId, 'Allow', event.methodArn);
+  } else {
+    policyDocument = generatePolicy(principalId, 'Deny', event.methodArn);
+  }
+
+  return {
+    principalId,
+    policyDocument
+  };
+};
+
+const generatePolicy = (principalId, effect, resource) => {
+  return {
       Version: '2012-10-17',
       Statement: [
           {
               Action: 'execute-api:Invoke',
-              Resource: [
-                  event.methodArn
-              ],
-              Effect: authStatus
+              Effect: effect,
+              Resource: resource
           }
       ]
-    }
   };
-  return response;
 };
