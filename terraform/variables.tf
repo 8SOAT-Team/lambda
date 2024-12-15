e4e737c7e07cb21fbee207ab7b3227df96575120
Fernanda Vecchiati Beato <feehvecch@gmail.com>
2024-12-15 13:05:50 -0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,9 @@ variable "lambda_authorizer_arn" {
   description = "ARN da função Lambda Authorizer"
   type        = string
 }
+
+variable "aws_region" {
+  description = "AWS region"
+  type = string
+  default = "us-east-1"
+}
