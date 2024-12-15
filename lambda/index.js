391550af10b824aeb430221ba2f81fb278240227
Fernanda Vecchiati Beato <feehvecch@gmail.com>
2024-12-15 11:39:10 -0300

    Create index.js

---
+++ lambda/index.js
@@ -0,0 +1,37 @@
+const defaultPassword = 'Fast*Order'; 
+
+export const handler = async (event) => {
+  const cpf = event.headers['cpf'];
+  const authorizationHeader = event.headers['Authorization'];
+
+  let policyDocument;
+  const principalId = 'user';
+
+  if (!cpf) {
+    policyDocument = generatePolicy(principalId, 'Allow', event.methodArn);
+  } else if (cpf) {
+    policyDocument = generatePolicy(principalId, 'Allow', event.methodArn);
+  } else if (authorizationHeader && authorizationHeader === `Basic ${Buffer.from(defaultPassword).toString('base64')}`) {
+    policyDocument = generatePolicy(principalId, 'Allow', event.methodArn);
+  } else {
+    policyDocument = generatePolicy(principalId, 'Deny', event.methodArn);
+  }
+
+  return {
+    principalId,
+    policyDocument
+  };
+};
+
+const generatePolicy = (principalId, effect, resource) => {
+  return {
+      Version: '2012-10-17',
+      Statement: [
+          {
+              Action: 'execute-api:Invoke',
+              Effect: effect,
+              Resource: resource
+          }
+      ]
+  };
+};
