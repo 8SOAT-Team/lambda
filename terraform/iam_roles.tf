30201ecedb9585065a015db00447fc0cfdd317ee
Fernanda Vecchiati Beato <feehvecch@gmail.com>
2024-12-15 14:50:47 -0300

    Update iam_roles.tf

--- terraform/iam_roles.tf
+++ terraform/iam_roles.tf
@@ -1,16 +1,9 @@
-resource "aws_iam_role" "lambda_exec" {
-  name = "lambda_exec_role"
-
-  assume_role_policy = jsonencode({
+resource "aws_iam_policy" "iam_create_role_policy" {
+  name        = "iam_create_role_policy"
+  description = "Permite criar roles IAM"
+  policy      = jsonencode({
     Version = "2012-10-17"
     Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      },
       {
         Effect   = "Allow"
         Action   = "iam:CreateRole"
@@ -28,6 +21,28 @@ resource "aws_iam_role" "lambda_exec" {
       }
     ]
   })
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda_exec_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_create_role_policy" {
+  role       = "lambda_exec_role"
+  policy_arn = aws_iam_policy.iam_create_role_policy.arn
 }
 
 output "lambda_exec_role_arn" {
