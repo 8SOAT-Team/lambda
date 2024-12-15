5c112fd25af29d21a8f1779a4777c658aaea96f6
Fernanda Vecchiati Beato <feehvecch@gmail.com>
2024-12-15 13:37:49 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,16 +11,16 @@ provider "aws" {
 }
 
 module "iam_roles" {
-  source = "/iam_roles.tf"
+  source = "iam_roles.tf"
 }
 
 module "lambda" {
-  source = "/lambda.tf"
+  source = "lambda.tf"
   lambda_role = module.iam_roles.lambda_exec_role_arn
 }
 
 module "api_gateway" {
-  source = "/api_gateway.tf"
+  source = "api_gateway.tf"
   swagger_file = "swagger.json"
   lambda_authorizer_arn = module.lambda.lambda_arn
 }
