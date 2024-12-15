824b755369b5b192341ad01b50040bb1ddab7373
Fernanda Vecchiati Beato <feehvecch@gmail.com>
2024-12-15 14:20:21 -0300

    Update api_gateway.tf

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -11,7 +11,7 @@ resource "aws_api_gateway_rest_api" "api" {
 resource "aws_api_gateway_authorizer" "lambda_authorizer" {
   name                    = "custom-lambda-authorizer"
   rest_api_id             = aws_api_gateway_rest_api.api.id
-  authorizer_uri          = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/${var.lambda_authorizer_arn}/invocations"
+  authorizer_uri          = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/${aws_lambda_function.lambda.arn}/invocations"
   type                    = "TOKEN"
   identity_source         = "method.request.header.Authorization"
   authorizer_result_ttl_in_seconds = 300
